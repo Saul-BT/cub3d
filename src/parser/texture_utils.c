d2a28c2864dd149c0aca975477a2ebfdd21002a6
Saul Blanco tejero <sblanco-@c1r9s3.42madrid.com>
2025-12-15 19:02:01 +0100

    fix: color validations

--- src/parser/texture_utils.c
+++ src/parser/texture_utils.c
@@ -6,7 +6,7 @@
 /*   By: sblanco- <sblanco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 18:38:06 by sblanco-          #+#    #+#             */
-/*   Updated: 2025/12/14 21:23:59 by sblanco-         ###   ########.fr       */
+/*   Updated: 2025/12/15 18:56:42 by sblanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,30 @@ static void	color_values_free(char **values)
 	free(values);
 }
 
-static int	get_color_value(char *str_value)
+static int	get_valid_number(char *str_num)
 {
+	size_t	i;
 	long	value;
 	char	*trimmed;
 
-	trimmed = ft_strtrim(str_value, " ");
+	i = 0;
+	trimmed = ft_strtrim(str_num, " ");
+	while (trimmed[i])
+	{
+		if (!ft_isdigit(trimmed[i]))
+			return (free(trimmed), -1);
+		i++;
+	}
 	value = ft_atoi(trimmed);
 	free(trimmed);
+	return ((int)value);
+}
+
+static int	get_color_value(char *str_value)
+{
+	int	value;
+
+	value = get_valid_number(str_value);
 	if (value > 255 || value < 0)
 		return (-1);
 	return ((int)value);
@@ -58,13 +74,13 @@ bool	set_valid_color(char *color, uint32_t *value)
 		return (false);
 	if (count_color_values(values) != 3)
 		return (color_values_free(values), false);
-	red = get_color_value(color);
+	red = get_color_value(values[0]);
 	if (red == -1)
 		return (color_values_free(values), false);
-	green = get_color_value(color);
+	green = get_color_value(values[1]);
 	if (green == -1)
 		return (color_values_free(values), false);
-	blue = get_color_value(color);
+	blue = get_color_value(values[2]);
 	if (blue == -1)
 		return (color_values_free(values), false);
 	*value |= (red << 24 | green << 16 | blue << 8);
